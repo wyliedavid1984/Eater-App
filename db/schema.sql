9fdd47b83e5f4f253734c3e4b83e37bdaa9f4868
wylie <wyliedavid1984@gmail.com>
2021-01-12 09:57:48 -0500

    created schema and seed data for db

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS burgerdb;
+
+CREATE DATABASE burgerdb;
+
+USE burgerdb;
+
+CREATE TABLE burgers(
+id INT NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(30),
+devoured BOOLEAN DEFAULT false,
+PRIMARY KEY (id)
+);
